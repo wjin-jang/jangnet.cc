ac4e7823c5b21062a0a4fcf4729868ab95577079
woojin <woojin.email@gmail.com>
2026-03-09 12:03:25 +0000

    3js script

---
+++ posts/y0-fmcc2-w3-transformations/main.js
@@ -0,0 +1,67 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 30, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+//const controls = new OrbitControls( camera, renderer.domElement );
+const radiusMultiplier = 12;
+
+let waves = [];
+
+for(let x = 0; x < 24; x++){
+  let geometry = createWaveGeometry( x*radiusMultiplier );
+  let material = new THREE.LineBasicMaterial( { color: 0xffffff } );
+  let wave = new THREE.Line( geometry, material );
+
+  scene.add( wave )
+  waves.push( {obj: wave, radius: x*radiusMultiplier } )
+}
+
+camera.position.x = 0.01;
+camera.position.y = 0.01;
+camera.position.z = 0.01;
+//controls.update();
+
+function animate( time ) {
+  camera.rotateY(0.001);
+  //controls.update();
+
+  renderer.render( scene, camera );
+
+  //console.log(`${camera.position.x} ${camera.position.y} ${camera.position.z}`)
+}
+renderer.setAnimationLoop( animate );
+
+function createWaveGeometry( radius ) {
+  let points = [];
+  let circleResolution = 0.001;
+
+  for(let angle = 0; angle < 2*Math.PI; angle += circleResolution){
+    let freq = 60;
+    let amp = 200/radius;
+    let period = 0;
+
+    let y = Math.sin(angle * freq + period) * amp;
+
+    let x = radius*Math.cos(angle);
+    let z = radius*Math.sin(angle);
+
+
+    points.push( new THREE.Vector3( x , y , z ) );
+  }
+
+  let geometry = new THREE.BufferGeometry().setFromPoints( points );
+
+  return geometry
+}
+
+function createWaveMaterial( radius ) {
+  let material = new THREE.LineBasicMaterial( { color : `hsl(0, 100%, ${radius}%)` } );
+
+  return material;
+}
